90dc87b42e54beb719a59ccd2257a2dafaf3acb1
arielshaulov <arielshaulo@gmail.com>
2024-07-27 13:30:29 +0300

    untrack models folder

--- models/imagebindmodel/imagebind/__init__.py
+++
@@ -1,3 +0,0 @@
-from models.imagebindmodel.imagebind import data
-from models.imagebindmodel.imagebind.models import imagebind_model
-from models.imagebindmodel.imagebind.models.imagebind_model import ModalityType
