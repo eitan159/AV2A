3126f377f2f6607a5164b8660f1ee9189f1bb530
eitan159 <eitan.b09@gmail.com>
2024-08-10 19:09:27 +0300

    first

--- datasets.py
+++ datasets.py
@@ -5,14 +5,40 @@ import torch
 import random
 from PIL import Image
 import os
-import pandas as pd
+import numpy as np
 from utils.video_pp import suppress_output
+import torchaudio
+import decord
+from decord import VideoReader, cpu
 
-def remove_files_from_dir(dir_path):
-    for filename in os.listdir(dir_path):
-        file_path = os.path.join(dir_path, filename)
-        if os.path.isfile(file_path):
-            os.remove(file_path)
+torchaudio.set_audio_backend("soundfile")
+decord.bridge.set_bridge('torch')
+
+
+import random
+
+def random_choose_indices(indices, step_size):
+    """
+    Randomly choose one index from each interval of the given step size.
+
+    Args:
+        indices (list or np.array): Array of indices (e.g., frame numbers).
+        step_size (int): The size of each step/interval from which one index will be randomly chosen.
+
+    Returns:
+        list: List of randomly chosen indices.
+    """
+    chosen_indices = []
+    # Loop through indices in steps of `step_size`
+    for i in range(0, len(indices), step_size):
+        # Define the interval
+        interval = indices[i:i + step_size]
+        # Randomly select one index from the interval
+        if interval:  # Ensure interval is not empty
+            chosen_index = random.choice(interval)
+            chosen_indices.append(chosen_index)
+    
+    return chosen_indices
 
 class AVE(Dataset):
     def __init__(self, video_dir_path, annotations_file_path, frames_transforms=None, 
@@ -79,15 +105,16 @@ class AVE(Dataset):
         return frames, self.audio_dir, self.video_annotation_dict[video_id], video_id
 
 class LLP(Dataset):
-    def __init__(self, video_dir_path, frames_transforms=None, 
+    def __init__(self, video_dir_path, audio_dir_path, frames_transforms=None, 
                  sample_audio_sec=2) -> None:
         self.video_dir_path = video_dir_path
+        self.audio_dir_path = audio_dir_path
         self.frames_transforms = frames_transforms
         self.sample_audio_sec = sample_audio_sec
         self.audio_dir = "sample_audio_chunks"
-        self.videos_ids = [video_id for video_id in os.listdir(video_dir_path) 
+        self.videos_ids = [video_id.replace(".mp4", "") for video_id in os.listdir(video_dir_path) 
                            if os.path.splitext(os.path.join(self.video_dir_path, video_id))[1] == '.mp4']
-                             
+
         
     def __len__(self):
         return len(self.videos_ids)
@@ -95,38 +122,20 @@ class LLP(Dataset):
     def __getitem__(self, idx):
         video_id = self.videos_ids[idx]
         video_path = os.path.join(self.video_dir_path, video_id)
+        audio_path = os.path.join(self.audio_dir_path, video_id)
+        
         try:
             video = VideoFileClip(video_path)
         except:
             print(video_path)
 
-        audio = video.audio
+        waveform_and_sr = torchaudio.load(audio_path)    
 
-        # Save the audio to a file
-        if not os.path.exists(self.audio_dir):
-            os.makedirs(self.audio_dir)
-        remove_files_from_dir(self.audio_dir)
+        decord_vr = VideoReader(video_path, ctx=cpu(0))
+        duration = len(decord_vr) // 10 # 10 sec videos
+        num_frames = 8 # language bind param 
+        frame_id_list = np.linspace(0, duration-1, num_frames, dtype=int)
+        video_data = decord_vr.get_batch(frame_id_list)
+        
 
-        for i, t in enumerate(range(0, int(audio.duration), self.sample_audio_sec)):
-            end_time = min(t + self.sample_audio_sec, audio.duration)
-            audio_chunk = audio.subclip(t_start=t, t_end=end_time)
-            output_filename = f"{self.audio_dir}/output_segment_{i}.wav"
-
-            # audio_chunk = audio.subclip(t_start=t, t_end=t + self.sample_audio_sec)
-            suppressed_write_audiofile = suppress_output(audio_chunk.write_audiofile)
-            suppressed_write_audiofile(output_filename)            
-
-        frames = []
-        fps = int(video.fps)
-        for t in range(int(video.duration)):
-            start_frame = random.randint(1, fps - 1)
-            frame = video.get_frame(t + (start_frame / fps))
-            frame = Image.fromarray(frame) # not good but for now it's ok
-            if self.frames_transforms:
-                frame = self.frames_transforms(frame)
-
-            frames.append(frame)
-
-        frames = torch.stack(frames)
-
-        return frames, self.audio_dir, video_id
+        return video_data, waveform_and_sr, video_id
